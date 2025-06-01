2e04f77bfbd00c3ab0fa1446fa67946543f64555
nick16384 <theophil@pudelkern.com>
2025-06-01 22:08:22 +0200

    [WIP] Adding file selection dialogs for source code and executable files

--- src/Avalonia/TinyProcVisualizer/Views/MainWindow.axaml.cs
+++ src/Avalonia/TinyProcVisualizer/Views/MainWindow.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using AvaloniaHex.Document;
 using System.IO;
+using Avalonia.Platform.Storage;
 
 namespace TinyProcVisualizer.Views;
 
@@ -13,7 +14,6 @@ public partial class MainWindow : Window
         InitializeComponent();
         Title = $"TinyProc CPU Emulator v{TinyProc.Application.GlobalData.TINYPROC_PROGRAM_VERSION_STR} Visualizer";
 
-        ReloadExecutableBinaryFile(null, null);
         SourceBinaryHexEditor.HexView.BytesPerLine = 8;
         //MainHexEditor.HexView.ColumnPadding = 20.0d;
         //MainHexEditor.HexView.FontSize = 22;
@@ -24,15 +24,46 @@ public partial class MainWindow : Window
     private void Button_InitCPU_OnClick(object? sender, RoutedEventArgs e)
     {
         Console.WriteLine("Initializing new CPU");
+        string? binaryExecutableFilePath = TextBox_BinaryExecutableFilePath.Text;
+        if (binaryExecutableFilePath == null)
+        {
+            // TODO: Show error in message dialog
+            // See https://github.com/AvaloniaCommunity/MessageBox.Avalonia
+            Console.Error.WriteLine("Cannot initialize CPU: Missing binary file.");
+            return;
+        }
+        TinyProc.Application.ExecutionContainer.Initialize(
+            new TinyProc.Application.ExecutableWrapper(binaryExecutableFilePath));
+
         Button_InitCPU.IsEnabled = false;
         Button_ReloadExecutable.IsEnabled = true;
         Button_CPUStepSingleCycle.IsEnabled = true;
 
-        TinyProc.Application.ExecutionContainer.Initialize(new TinyProc.Application.ExecutableWrapper("../../../Test Programs/ASMv2/Alphabet.lltp32.bin"));
         // TODO: Add real-time updating MemoryBinaryDocument for CPU RAM
     }
 
-    private MemoryBinaryDocument _currentBinFile;
+    private async void Button_OpenAssemblySourceFilePath_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open Assembly Source Code file...",
+            AllowMultiple = false
+        });
+
+        if (files.Count >= 1)
+        {
+            Console.WriteLine("Selected assembly source file: " + files[0].Name);
+            /*await using var stream = await files[0].OpenReadAsync();
+            using var streamReader = new StreamReader(stream);
+            string assemblySourceFileContent = await streamReader.ReadToEndAsync();*/
+            TextBox_AssemblySourceFilePath.Text = files[0].Path.AbsolutePath;
+        }
+    }
+    private void Button_OpenBinaryExecutableFilePath_OnClick(object? sender, RoutedEventArgs e)
+    {
+
+    }
 
     private void ReloadExecutableBinaryFile(object? sender, RoutedEventArgs e)
     {
