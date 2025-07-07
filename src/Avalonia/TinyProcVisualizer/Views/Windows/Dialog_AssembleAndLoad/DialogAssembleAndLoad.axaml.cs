d4d649390be99b253c1981cc3624cd7a7db38ec2
nick16384 <theophil@pudelkern.com>
2025-07-07 20:14:27 +0200

    Fixed compile error due to unfinished assemble and load

--- src/Avalonia/TinyProcVisualizer/Views/Windows/Dialog_AssembleAndLoad/DialogAssembleAndLoad.axaml.cs
+++ src/Avalonia/TinyProcVisualizer/Views/Windows/Dialog_AssembleAndLoad/DialogAssembleAndLoad.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace TinyProcVisualizer.Views.Windows.Dialog_AssembleAndLoad;
 
@@ -7,5 +9,20 @@ public partial class DialogAssembleAndLoad : Window
     public DialogAssembleAndLoad()
     {
         InitializeComponent();
+    }
+
+    public void Button_SetMinLoadAddress_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Button_SetMaxLoadAddress_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Button_SubmitLoadAddress_OnClick(object? sender, RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
